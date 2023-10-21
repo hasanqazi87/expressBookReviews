f59b6f8d3d2bd1f8f61e81d488eb43bfddc201b3
Hasan Qazi <hasanqazi87@gmail.com>
2023-10-21 13:39:46 -0400

    update express final project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,40 +20,56 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    return res.send(JSON.stringify(books));
+public_users.get('/', (req, res) => {
+    return new Promise((resolve, reject) => {
+        resolve(JSON.stringify(books));
+    }).then((successMsg) => {
+        res.send(successMsg);
+    });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    const book = books[isbn]
-    if (book) return res.send(JSON.stringify(books[req.params.isbn]));
-    return res.send(`No book with ISBN ${isbn}!`);
+    return new Promise((resolve, reject) => {
+        const isbn = req.params.isbn;
+        const book = books[isbn];
+        let msg = book ? JSON.stringify(book) : `No book with ISBN ${isbn}!`;
+        resolve(msg);
+    }).then((successMsg) => {
+        res.send(successMsg);
+    });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    let filteredBooks = [];
-    for (let isbn in books) {
-        let book = books[isbn];
-        if (book.author.toLowerCase().search(author.toLowerCase()) != -1) filteredBooks.push(book);
-    }
-    if (filteredBooks.length > 0) return res.send(JSON.stringify(filteredBooks));
-    return res.send(`No books found with author ${author}!`);
+    return new Promise((resolve, reject) => {
+        const author = req.params.author;
+        let filteredBooks = [];
+        for (let isbn in books) {
+            let book = books[isbn];
+            if (book.author.toLowerCase().search(author.toLowerCase()) != -1) filteredBooks.push(book);
+        }
+        let msg = filteredBooks.length > 0 ? JSON.stringify(filteredBooks) : `No books found with author ${author}!`;
+        resolve(msg);
+    }).then((successMsg) => {
+        res.send(successMsg);
+    });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    let filteredBooks = [];
-    for (let isbn in books) {
-        let book = books[isbn];
-        if (book.title.toLowerCase().search(title.toLowerCase()) != -1) filteredBooks.push(book);
-    }
-    if (filteredBooks.length > 0) return res.send(JSON.stringify(filteredBooks));
-    return res.send(`No books with title ${title}!`);
+    return new Promise((resolve, reject) => {
+        const title = req.params.title;
+        let filteredBooks = [];
+        for (let isbn in books) {
+            let book = books[isbn];
+            if (book.title.toLowerCase().search(title.toLowerCase()) != -1) filteredBooks.push(book);
+        }
+        let msg = filteredBooks.length > 0 ? JSON.stringify(filteredBooks) : `No books with title ${title}!`;
+        resolve(msg);
+    }).then((successMsg) => {
+        res.send(successMsg);
+    });
 });
 
 //  Get book review
